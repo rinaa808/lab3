e4c48429aeabe8af4a773ef6e37f8dfc6be6846e
rinaa808 <kyzuuer@mail.ru>
2023-05-17 20:16:23 +0300

    feat: operation add

--- main.py
+++ main.py
@@ -7,5 +7,16 @@ def main():
     list_of_operations.clear()
     return render_template('main.html')
 
+@app.route("/calculate", methods=["POST"])
+def calculate():
+    number_one = request.form["first_number"]
+    number_two = request.form["second_number"]
+    operation = request.form["operation"]
+
+    if operation == "add":
+        result = float(number_one) + float(number_two)
+        list_of_operations.append('{0} + {1} = {2}'.format(number_one, number_two, result))
+        return render_template("main.html", result=result, operations=list_of_operations)
+
 if __name__ == '__main__':
     app.run(debug=True)
